27b3326c52ece9dcc445fc1226c84cb063b35128
Akshat-yadav2009 <akshatsinghaniya2009@gmail.com>
2026-04-05 23:37:42 +0530

    Add files via upload

---
+++ day1_variables.py/day_project.py/project.py
@@ -0,0 +1,50 @@
+"""Bio card"""
+
+print("="*15)
+print("My bio card")
+print("="*15)
+
+Name="Harsh vadana sharma"
+Age=30
+height=1.71
+city="Bhopal"
+is_student=False
+Companyshare=60.465
+
+print("Name:", Name)
+print("Age:", Age)
+print("Height:", height)
+print("City:", city)
+print("Is Student:", is_student)
+print("Company Share:", Companyshare)
+
+print("="*15)
+
+print(type(Name))
+print(type(Age))
+print(type(height))
+print(type(city))
+print(type(is_student))
+print(type(Companyshare))
+
+"""Simple calculator"""
+
+print("="*15)
+print("My simple calculator setup")
+print("="*15)
+
+Num1=19
+Num2=15
+
+Addition=Num1+Num2
+subtracation=Num1-Num2
+Multipliation=Num1*Num2
+division=Num1/Num2
+
+print("Addition:",Addition)
+print("Subtraction:",subtracation)
+print("Multiplication:",Multipliation)
+print("Division:",division)
+
+print("="*15)
+
---
+++ day1_variables.py/main.py
@@ -0,0 +1,54 @@
+"""DAY1:-
+what to study?
+1) variables 
+2) basic data types """
+
+"""Variable:-A container where we store values"""
+Akshat=12
+ak="Rahul"
+pi=3.14
+is_akshat=True
+
+
+"""basic data types
+1) integer:- where we can store numbers without a decimal all +ve aur -ve """
+Age=12
+vijay=19
+
+"""2) float:- where we can store decimel numbers """
+pi=3.14
+price=19.99
+height=15.5
+  
+
+"""3) boolean logic= it is uesd as a condition like you are above 12 of below or any other condition"""
+is_girl=True
+has_discount=False
+is_number=False
+
+"""4)string:-a word or any other sentence coverin Quotes"""
+ak="rahul"
+name="alice"
+email="Abc@gmail.com"
+
+
+"""Exercise"""
+student_name="Akshat"
+age=16
+gpa=8.79
+is_enrolled=True
+completed=12
+completionrate=90
+
+print("Name",student_name)
+print("type",type(student_name))
+print("age",age)
+print("age",type(age))
+print("gpa",gpa)
+print("gpa",type(gpa))
+print("is_enrolled",is_enrolled)
+print("is_enrolled",type(is_enrolled))
+print("completed",completed)
+print("completed",type(completed))
+print("completionrate",completionrate)
+print("completionrate",type(completionrate))
